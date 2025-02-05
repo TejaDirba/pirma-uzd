f9bbc24d658dc4f755bdb7623ff2bd31f0acb2fe
TejaDirba <teja.tupinyte@gmail.com>
2025-02-05 16:49:39 +0200

    Create mano_lib.h

---
+++ mano_lib.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+
+struct Stud{
+	string var="a", pav="a";
+	vector <int> paz;
+	int egz=9;
+}
