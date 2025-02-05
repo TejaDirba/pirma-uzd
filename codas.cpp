963d3cba24fe2a994ee66524118df9f7cb4d5902
TejaDirba <teja.tupinyte@gmail.com>
2025-02-05 16:59:18 +0200

    Update codas.cpp

--- codas.cpp
+++ codas.cpp
@@ -3,9 +3,15 @@ vector<Stud> grupe;
 //while skaicius teigiamas ir imanomas and stuff
 for (int i=0; i<5; i++){
 	Stud laik;
+	cout<<"irasykite varda: ";
 	cin>>laik.var;
+	cout<<endl;
+	cout<<"irasykite pavarde: ";
 	cin>>laik.pav;
+	cout<<endl;
+	cout<<"irasykite egzamino ivertinima: ";
 	cin>>laik.egz;
+	cout<<endl;
 	grupe.pushback(laik)
 }
 
