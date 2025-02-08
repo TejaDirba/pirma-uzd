1b05275bb0f4616e6c9d0a8e91c487aca65bb876
TejaDirba <teja.tupinyte@gmail.com>
2025-02-08 17:34:47 +0200

    includeinau lib

--- codas.cpp
+++ codas.cpp
@@ -1,22 +1,23 @@
+#include 'mano_lib.h'
 int main(){
-vector<Stud> grupe;
-//while skaicius teigiamas ir imanomas and stuff
-for (int i=0; i<5; i++){
-	Stud laik;
-	cout<<"irasykite varda: ";
-	cin>>laik.var;
-	cout<<endl;
-	cout<<"irasykite pavarde: ";
-	cin>>laik.pav;
-	cout<<endl;
-	cout<<"irasykite egzamino ivertinima: ";
-	cin>>laik.egz;
-	cout<<endl;
-	grupe.pushback(laik)
-}
+	vector<Stud> grupe;
+	//while skaicius teigiamas ir imanomas and stuff
+	for (int i=0; i<5; i++){
+		Stud laik;
+		cout<<"irasykite varda: ";
+		cin>>laik.var;
+		cout<<endl;
+		cout<<"irasykite pavarde: ";
+		cin>>laik.pav;
+		cout<<endl;
+		cout<<"irasykite egzamino ivertinima: ";
+		cin>>laik.egz;
+		cout<<endl;
+		grupe.pushback(laik)
+	}
 
-for (auto n :grupe){
-	cout<<n.pav<<""<<n.var<<endl;
-}
+	for (auto n :grupe){
+		cout<<n.pav<<""<<n.var<<endl;
+	}
 }
 
